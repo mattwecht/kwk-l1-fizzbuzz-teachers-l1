37116ecd837721cb482cc532a2b6aabf0ce490f1
mattwecht <mattwecht@gmail.com>
2018-07-24 18:45:59 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,12 @@
 def fizzbuzz (int)
+  s =""
   # if int %5 == 0 && int %3 == 0
   #   return "FizzBuzz"
   # end
   if int % 3 == 0 # if the number int is divisible by 3
-    return "Fizz" # Go fizz
+    s = s+ "Fizz" # Go fizz
   end
   if int % 5 == 0
-  return "Buzz"
-else
-  return nil
+  s = s+ "Buzz"
 end
 end

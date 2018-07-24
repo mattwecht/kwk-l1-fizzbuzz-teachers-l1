12d3bb4e6c8f89cd5f04c850184aab06d6066d02
mattwecht <mattwecht@gmail.com>
2018-07-24 18:47:14 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,8 +5,7 @@ def fizzbuzz (int)
   # end
   if int % 3 == 0 # if the number int is divisible by 3
     s = s+ "Fizz" # Go fizz
-  end
-  if int % 5 == 0
+  elsif int % 5 == 0
     s = s+ "Buzz"
   else
     return nil
